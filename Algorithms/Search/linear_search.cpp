1136c6af089c75e10b90b48148cb893b22e246a8
Q.Jhone <1148235243@qq.com>
2018-11-09 18:38:37 +0800

    Create linear_search.cpp

---
+++ Algorithms/Search/linear_search.cpp
@@ -0,0 +1,8 @@
+/*
+Linear search is used on a collections of items. 
+It relies on the technique of traversing a list from start to end 
+by exploring properties of all the elements that are found on the way.
+he time complexity of the linear search is O(N) because each element in an array is compared only once.
+*/
+
+
