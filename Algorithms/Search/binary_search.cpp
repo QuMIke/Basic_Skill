5d047a9b201badc89b9199107e008581384ca951
Q.Jhone <1148235243@qq.com>
2018-11-09 21:25:46 +0800

    Create binary_search.cpp

---
+++ Algorithms/Search/binary_search.cpp
@@ -0,0 +1,8 @@
+/*
+Binary search is the most popular Search algorithmh. 
+It is efficient and also one of the most commonly used techniques that is used to solve problems.
+It is used to perform operations on a sorted set.
+As we dispose off one part of the search case during every step of binary search, 
+and perform the search operation on the other half, this results in a worst case time complexity of O(log2N).
+*/
+
