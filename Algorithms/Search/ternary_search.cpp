dd423b7bf2eddef6cdf66a1e0a7d2eba5983a59d
Q.Jhone <1148235243@qq.com>
2018-11-10 13:22:54 +0800

    Create ternary_search.cpp

---
+++ Algorithms/Search/ternary_search.cpp
@@ -0,0 +1,12 @@
+/*
+Like linear search and binary search, 
+ternary search is a searching techniquethat is used to determine the position of a specific value in an array. 
+In binary search, the sorted array is divided into two parts while in ternary search,
+it is divided into  parts and then you determine in which part the element exists.
+Ternary search, like binary search, is a divide-and-conquer algorithm. 
+It is mandatory for the array (in which you will search for an element) to be sorted before you begin the search. 
+In this search, after each iteration it neglects ⅓part of the array and repeats the same operations on the remaining ⅔.
+Its time complexity is O(log3N).
+*/
+
+
