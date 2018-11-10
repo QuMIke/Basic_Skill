7654dbdc6107f4151561abf5cec28f19e507979b
Q.Jhone <1148235243@qq.com>
2018-11-10 16:28:55 +0800

    Create bubble_sort.cpp

---
+++ Algorithms/Sorting/bubble_sort.cpp
@@ -0,0 +1,5 @@
+/*
+Bubble sort is based on the idea of repeatedly comparing pairs of adjacent elements 
+and then swapping their positions if they exist in the wrong order.The complexity of bubble sort is  
+O(N*N)in both worst and average cases, because the entire array needs to be iterated for every element.
+*/
